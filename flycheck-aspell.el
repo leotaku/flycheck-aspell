01e8b4af3b5f086948fcf7bfcb18775252676f94
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-08-11 06:32:57 +0200

    Add short commentary to flycheck-aspell

--- flycheck-aspell.el
+++ flycheck-aspell.el
@@ -26,6 +26,10 @@
 
 ;;; Commentary:
 
+;; This file provides a flymake-based spell-checker for documents
+;; using GNU Aspell as a backend.  Please consult the README.md file
+;; contained in this directory for further information.
+
 (require 'flycheck)
 (require 'ispell)
 
