526af64f5db7b2c1c346c001d8d3ad9e835d4f44
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-08-11 13:16:21 +0200

    Fix commentary section

--- flymake-aspell.el
+++ flymake-aspell.el
@@ -27,9 +27,9 @@
 ;;; Commentary:
 
 ;; This file provides a flymake-based spell-checker for documents
-;; using GNU Aspell as a backend. Enable it by adding the following to
-;; your init file. You must be running Emacs 26 or newer in order to
-;; use the new version of Flymake.
+;; using GNU Aspell as a backend.  Enable it by adding the following
+;; to your init file.  You must be running Emacs 26 or newer in order
+;; to use the new version of Flymake.
 
 ;;   (add-hook 'text-mode-hook #'flymake-aspell-setup)
 
